5b65f23a15adc101a3fd96a6e6e447cbc13aa1e3
leoromero1 <leoromero.bls@gmail.com>
2025-04-11 16:43:02 -0300

    Modifico el contexto, en este caso era la clase alumno. Creo la composicion. creo una estrategia por defecto y una funcion para cambiar de estratgia

--- Models/Alumno.cs
+++ Models/Alumno.cs
@@ -1,13 +1,19 @@
+using Practicas.Interfaces;
+using Practicas.Strategy;
+
 namespace Practicas.Models;
 public class Alumno : Persona
 {
     private int Legajo;
     private float Promedio;
 
+    private EstrategiaDeComparacion estrategia;
+
     public Alumno(string nombre, int dni, int legajo, float promedio) : base(nombre, dni)
     {
         Legajo = legajo;
         Promedio = promedio;
+        estrategia = new EstrategiaPorDni();
     }
     public int GetLegajo()
     {
@@ -16,5 +22,22 @@ public class Alumno : Persona
     public float GetPromedio()
     {
         return Promedio;
+    }
+    public void SetEstrategia(EstrategiaDeComparacion e)
+    {
+        estrategia = e;
+    }
+
+    public override bool SosIgual(Comparable c)
+    {
+        return estrategia.SosIgual(this, (Alumno)c);
+    }
+    public override bool SosMenor(Comparable c)
+    {
+        return estrategia.SosMenor(this, (Alumno)c);
+    }
+    public override bool SosMayor(Comparable c)
+    {
+        return estrategia.SosMayor(this, (Alumno)c);
     }
 }
