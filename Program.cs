e5771175847ecc11becedd0c47f1e3d9aa1dd610
leoromero1 <leoromero.bls@gmail.com>
2025-04-11 15:39:59 -0300

    Ejercicio 9: Modifique el módulo principal para crear una ColeccionMultiple e informe con esta colección.

--- Program.cs
+++ Program.cs
@@ -7,11 +7,15 @@ class Program
     {
         Pila pila = new Pila();
         Cola cola = new Cola();
+        ColeccionMultiple coleccion = new ColeccionMultiple(pila, cola);
         Helpers.Llenar(pila);
         Helpers.Llenar(cola);
         Console.Write("PILA:");
         Helpers.Informar(pila);
         Console.Write("Cola:");
         Helpers.Informar(cola);
+        Console.Write("Coleccion:");
+        Helpers.Informar(coleccion);
+
     }
 }
