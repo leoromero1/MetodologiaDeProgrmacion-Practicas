898cadfc617a228d1f1245d166ddb0855196c901
leoromero1 <leoromero.bls@gmail.com>
2025-04-11 15:31:29 -0300

    Ejercicio 7: Implemente un módulo principal que cree una Pila y una Cola, que las llene y que invoque a la función informar con la pila y cola creadas.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,17 @@
-﻿class Program
+﻿using Practicas;
+using Practicas.Models;
+
+class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Leo Romero");
+        Pila pila = new Pila();
+        Cola cola = new Cola();
+        Helpers.Llenar(pila);
+        Helpers.Llenar(cola);
+        Console.Write("PILA:");
+        Helpers.Informar(pila);
+        Console.Write("Cola:");
+        Helpers.Informar(cola);
     }
 }
