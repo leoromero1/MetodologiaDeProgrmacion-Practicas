e167b02f13700dfef54a436578b0e73df3ea3f21
leoromero1 <leoromero.bls@gmail.com>
2025-04-11 15:08:02 -0300

    ejercicio 1: Implemente la siguiente interface que representa cualquier objeto que sabe comparase con otro objeto de su “mismo tipo”

---
+++ Interfaces/Comparable.cs
@@ -0,0 +1,7 @@
+namespace Practicas.Interfaces;
+public interface Comparable
+{
+    bool SosIgual(Comparable c);
+    bool SosMenor(Comparable c);
+    bool SosMayor(Comparable c);
+}
--- obj/Debug/net8.0/Practicas.AssemblyInfo.cs
+++ obj/Debug/net8.0/Practicas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practicas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+074a8d4fd6c2f6607f3f8451996356e0168b2770")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practicas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practicas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
