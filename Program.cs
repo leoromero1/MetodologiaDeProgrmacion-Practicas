a477713e6501e0fabe14c1cf694677f713b47704
leoromero1 <leoromero.bls@gmail.com>
2025-04-23 08:58:10 -0300

    Elimine las carpetas bin y obj que tenian seguimiento

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
-﻿using Practicas;
+﻿
 using Practicas.Models;
-using Practicas.Strategy;
-
 class Program
 {
     public static void Main(string[] args)
@@ -9,7 +7,7 @@ class Program
         GeneradorDeDatosAleatorios generador = new GeneradorDeDatosAleatorios();
         int numeroAleatorio = generador.NumeroAleatorio(10);
         Console.WriteLine($"numeroAleatorio: {numeroAleatorio}");
-        string cadenaAleatoria = generador.stringAleatorio(numeroAleatorio);
+        string cadenaAleatoria = generador.StringAleatorio(numeroAleatorio);
         Console.WriteLine($"cadenaAleatoria: {cadenaAleatoria}");
     }
 }
