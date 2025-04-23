596905a8e4a8cd2155abdc34c50b25d4216e949c
leoromero1 <leoromero.bls@gmail.com>
2025-04-23 08:53:05 -0300

    Practica 3-Ejercicio 2: Implemente la clase GeneradorDeDatosAleatorios

--- Program.cs
+++ Program.cs
@@ -6,25 +6,10 @@ class Program
 {
     public static void Main(string[] args)
     {
-        Pila pila = new Pila();
-        Cola cola = new Cola();
-        Conjunto conjunto = new Conjunto();
-        Helpers.LlenarAlumnos(pila);
-        Helpers.LlenarAlumnos(cola);
-        Helpers.LlenarAlumnos(conjunto);
-        Console.WriteLine("Documentos de Pila:");
-        Helpers.ImprimirDocumentos(pila);
-        Console.WriteLine("Documentos de Cola:");
-        Helpers.ImprimirDocumentos(cola);
-        Console.WriteLine("Documentos de Conjunto:");
-        Helpers.ImprimirDocumentos(conjunto);
-        Console.WriteLine("Cambiando estrategia a EstrategiaPorDni...");
-        Helpers.CambiarEstrategia(pila, new EstrategiaPorDni());
-        Console.WriteLine("Con estrategia por DNI:");
-        Helpers.Informar(pila);
-        Console.WriteLine("Cambiando estrategia a EstrategiaPorPromedio...");
-        Helpers.CambiarEstrategia(pila, new EstrategiaPorPromedio());
-        Console.WriteLine("Con estrategia por Promedio:");
-        Helpers.Informar(pila);
+        GeneradorDeDatosAleatorios generador = new GeneradorDeDatosAleatorios();
+        int numeroAleatorio = generador.NumeroAleatorio(10);
+        Console.WriteLine($"numeroAleatorio: {numeroAleatorio}");
+        string cadenaAleatoria = generador.stringAleatorio(numeroAleatorio);
+        Console.WriteLine($"cadenaAleatoria: {cadenaAleatoria}");
     }
 }
